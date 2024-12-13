bd4e61e14d71c023f455f729075bf36b98ceb8f1
VengeTH <prinzemikhailsadsad@gmail.com>
2024-12-13 12:36:25 +0800

    ongoing footer and fixed some layouts on contact us page

---
+++ footer2.php
@@ -0,0 +1,21 @@
+<div class="footer">
+    test
+</div>
+
+<style>
+    .footer{
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 1em;
+        background-color: black;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 100;
+        height: 10vh;
+        width: 100%;
+        margin-top: auto; /* Ensure the footer is pushed to the bottom */
+    }
+</style>
