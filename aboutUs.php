fc3dd1752c8517f61ac543837b071177a9deaef5
VengeTH <prinzemikhailsadsad@gmail.com>
2024-12-14 02:03:35 +0800

    upload ko na rin konting changes sa aboutus and deleteacc php

---
+++ aboutUs.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Management by MuseoAR</title>
+    <link rel="stylesheet" href="/css/aboutUs.css">
+    <link rel="icon" href="/img/logo.png" type="image/x-icon">
+</head>
+<body>
+    <?php
+        include "headerWhite.php";
+    ?>
+    content here
+    <?php
+        include "footer2.php";
+    ?>
+</body>
+</html>
--- deleteAcc.php
+++ deleteAcc.php
@@ -11,5 +11,32 @@
     <?php
         include "headerWhite.php";
     ?>
+    <div class="deleteAccUpper">
+        <h1>Delete Account</h1>
+    </div>
+    <div class="nilalaman">
+        <h1>Overview</h1>
+        <p>We take your privacy seriously. If you wish to delete your account and all associated data from our system, please follow the instructions below. Once your data is deleted, it cannot be recovered.</p>
+    </div>
+    <div class="steps">
+        <h1>How To Delete your Account and Data</h1>
+        <ol>
+            <li>Log in to your account: <span style="color:gray;">Visit <a href="organiss.free.nf" style="color:blue; text-decoration: none;"> Organiss Website</a> and log in with your credentials</span></li>
+            <li>Navigate to Account Settings: <span style="color:gray;">Once logged in, go to your profile or account settings page.
+            </span></li>
+            <li>Request Data Deletion: <span style="color:gray;">Look for the "Delete Account" option in the settings. Click on it to begin the deletion process.
+            </span></li>
+            <li>Confirm Your Deletion Request: <span style="color:gray;">You will be asked to confirm that you want to permanently delete your account and all personal data associated with it. After confirming, your account and data will be permanently deleted.</span></li>
+    </div>
+    <div class="importantNotes">
+        <h1>Important Notes</h1>
+        <ol>
+            <li>Data Deletion is Final: <span style="color:gray;">Once your data is deleted, it cannot be recovered.</span></li>
+            <li>Processing Time: <span style="color:gray;">Data deletion may take up to 48 hours to be fully processed.</span></li>
+        </ol>
+    </div>
+    <?php
+        include "footer2.php";
+    ?>
 </body>
 </html>
